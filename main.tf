8063fa6e23d285d7b932ef8c0622703387bbc7ec
Sandeep Silumula <sandeep.silumula@gmail.com>
2026-05-27 13:48:17 -0400

    Add main.tf for Terraform infrastructure

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "one" {
+  count                  = 4
+  ami                    = "ami-0ecb62995f68bb549"
+  instance_type          = "c7i-flex.large"
+  key_name               = "rahamdocker"
+  vpc_security_group_ids = ["sg-046cd16cdb7a78595"]
+  tags = {
+    Name = var.instance_names[count.index]
+  }
+}
+
+variable "instance_names" {
+  default = ["jenkins", "tomcat-1", "tomcat-2", "Monitoring server"]
+}
