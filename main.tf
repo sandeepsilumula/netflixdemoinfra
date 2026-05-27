d934cd6079ce5a458b803994e1c8508787f19fac
Sandeep Silumula <sandeep.silumula@gmail.com>
2026-05-27 17:56:11 -0400

    changed the config

--- main.tf
+++ main.tf
@@ -13,9 +13,9 @@ provider "aws" {
 resource "aws_security_group" "web_sg" {
   name        = "web-server-sg"
   description = "Security group for Netflix app servers"
-  # IMPORTANT: Ensure this VPC ID actually exists in the MEMBER account
   vpc_id      = "vpc-0a1c70c25aa611d04" 
 
+  # SSH Access
   ingress {
     from_port   = 22
     to_port     = 22
@@ -23,6 +23,31 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Jenkins / Tomcat
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Grafana
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Prometheus
+  ingress {
+    from_port   = 9090
+    to_port     = 9090
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound traffic
   egress {
     from_port   = 0
     to_port     = 0
@@ -35,7 +60,7 @@ resource "aws_security_group" "web_sg" {
 resource "aws_instance" "one" {
   count                  = 4
   ami                    = "ami-04aa00acb1165b32a"
-  instance_type          = "t2.medium"
+  instance_type          = "c7i-flex.large"
   key_name               = "DeOps-Admin"
   
   # Reference the security group created by Terraform above
